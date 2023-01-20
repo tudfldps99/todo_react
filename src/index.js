e1000b3acda2ef1179432615ecf39d94391a1059
tudfldps99 <tudfldps99@naver.com>
2023-01-20 15:47:23 +0900

    # 초기 설정

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />   // 컴포넌트 : 의미가 있는 태그들을 한곳에 모아놓은 부품
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
