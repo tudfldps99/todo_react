e4b98492eeb94d7735d6872d9306737745b6fd52
tudfldps99 <tudfldps99@naver.com>
2023-01-25 17:11:45 +0900

    # 집가서 할일 1 완료

--- src/components/todo/TodoHeader.js
+++ src/components/todo/TodoHeader.js
@@ -8,11 +8,14 @@ const TodoHeader = ({ todoList }) => {
     // todoList에서 done값이 flase인 객체들만 필터링
     const unDoneTodos = todoList.filter(todo => !todo.done);
 
-    // 할일 1. 현재 날짜로 바꿔보기
+    let now = new Date();
+    const week = ['일', '월', '화', '수', '목', '금', '토'];
+
+    // 할일 1. 현재 날짜로 바꿔보기 (완료)
     return (
         <header>
-            <h1>2023년 1월 25일</h1>
-            <h2 className='day'>수요일</h2>
+            <h1>{now.getFullYear()}년 {now.getMonth() + 1}월 {now.getDate()}일</h1>
+            <h2 className='day'>{week[now.getDay()]}요일</h2>
             <div className='tasks-left'>할 일 {unDoneTodos.length}개 남음</div>
         </header>      
     );
