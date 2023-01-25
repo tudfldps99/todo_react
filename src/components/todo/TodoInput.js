54137bf8afc4a74c520482e79228e2010aae1de1
tudfldps99 <tudfldps99@naver.com>
2023-01-25 16:17:04 +0900

    # 할 일 조회, 등록 서버 요청처리

--- src/components/todo/TodoInput.js
+++ src/components/todo/TodoInput.js
@@ -6,29 +6,88 @@ import cn from 'classnames';
 
 import './css/TodoInput.css';
 
-const TodoInput = () => {
+const TodoInput = ({ add }) => {        // 부모(TodoTemplate.js)가 보낸 addTodo 함수
 
     // todo-input 박스를 렌더링할지 여부
     const [open, setOpen] = useState(false);
+    
+    // 입력폼에 입력한 데이터들을 담을 상태변수
+    const [todo, setTodo] = useState({
+        title: '',
+        /*text: ''*/
+    });
 
     // todo-input 박스를 열고 닫는 클릭이벤트 핸들러
     const inputToggle = e => {
         setOpen(!open);
+    };
+
+    // 입력값을 실시간으로 상태변수 todo에 저장하는 change 핸들러
+    const titleChangeHandler = e => {
+
+        // typeof e.target.value = string
+        setTodo({
+            ...todo,        // 기존 todo데이터 복사 후 title 추가
+            title: e.target.value
+        });
+    };
+    /*
+    const blahblahHandler = e => {
+        setTodo({
+            ...todo,        // 기존 todo데이터 복사 후 text 추가
+            text: e.target.value
+        });
+    };
+    */
+
+    // 할 일 입력 후 엔터 치면 서버로 POST 요청을 보내는 이벤트 핸들러
+    const todoAddHandler = e => {
+        if (e.key === 'Enter') {
+
+            // 입력데이터들을 읽기
+            //console.log(todo);
+
+            // 서버 요청 보내기 --> 부모(TodoTemplate.js)에서 처리하는게 맞음
+            //                     자식은 부모에게 입력데이터를 콜백함수로 넘기면 됨
+            add(todo);
+
+            // 서버 요청 보낸 후 title 입력값 비우기
+            setTodo({
+                ...todo,
+                title: ''
+            });
+        };
+    };
+
+    // <form> 태그의 submit 을 멈추는 핸들러
+    const stopSubmit = e => {
+        e.preventDefault();     // 자동 submit 중지
     }
 
     return (
         <>
             { open &&
                 <div className='todo-input'>
-                    <form className='insert-form'>
+                    <form className='insert-form' onSubmit={stopSubmit}>
                         <input 
                             type='text' 
                             placeholder='할 일을 입력 후, Enter 를 누르세요'
-                            autoFocus  
+                            autoFocus 
+                            onKeyUp={todoAddHandler} 
+                            onChange={titleChangeHandler}
+                            value={todo.title}
                         />
+                        {/*
+                        <input
+                            type='text'
+                            placeholder='아무말'
+                            onChange={blahblahHandler}
+                        />
+                        */}
                     </form>
                 </div>
             }
+
             {/*  classNames 미사용 시 ) className={open ? 'begin-btn open' : 'begin-btn'} */}
             <button className={cn('begin-btn', {open})} onClick={() => inputToggle()}>    {/* cn('begin-btn', {open}) : open의 논리값에 따라 true면 기존 클래스에 붙음 */}
                 <MdAdd />
--- src/components/todo/TodoTemplate.js
+++ src/components/todo/TodoTemplate.js
@@ -1,5 +1,5 @@
 // 2023-01-25
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import TodoHeader from './TodoHeader'
 import TodoInput from './TodoInput'
@@ -10,35 +10,38 @@ import './css/TodoTemplate.css'
 
 const TodoTemplate = () => {
 
+    const API_BASE_URL = 'http://localhost:8080/api/todos';
+
     // 할일 api 데이터 
-    const todos = [
-        {
-            id: 1,
-            title: '아침 산책',
-            done: true
-        },
-        {
-            id: 2,
-            title: '오늘의 뉴스 읽기',
-            done: true
-        },
-        {
-            id: 3,
-            title: '샌드위치 사먹기',
-            done: false
-        },
-        {
-            id: 4,
-            title: '리액트 공부하기',
-            done: false
-        },
-    ];
+    const [todos, setTodos] = useState([]);
+
+    // 할일 등록 서버 요청  (POST에 대한 응답처리)
+    const addTodo = ( todo ) => {
+        fetch(API_BASE_URL, {
+            method: 'POST',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify(todo)          // todo : <form> 입력 데이터 --> 자식에게 있는 데이터 ==> callback 이용
+        })
+        .then(res => res.json())
+        .then(result => {
+            setTodos(result.todos);
+        });
+    };
+
+    // 렌더링 되자마자 할 일 => todos api GET 목록 호출
+    useEffect(() => {
+        fetch(API_BASE_URL)
+            .then(res => res.json())
+            .then(result => {
+                setTodos(result.todos);
+            });
+    }, []);
 
     return (
         <div className='todo-template'>
             <TodoHeader todoList={todos} />     {/* '할 일 x개 남음' 을 위해 TodoHeader에서도 todos 데이터 필요 */}
             <TodoMain todoList={todos} />
-            <TodoInput />
+            <TodoInput add={addTodo}/>      {/* 자식에서 부모에게 입력값을 콜백함수로 넘기기 위해 자식에게 addTodo 함수 보냄 */}
         </div>
     )
 }
